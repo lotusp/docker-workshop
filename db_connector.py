d3e3c4797a07b1b260b6f07f192cb70f3722a001
Guangguang <gordon.mgg@gmail.com>
2019-05-06 23:45:26 +0800

    [guangguang] add python script to connect to database

---
+++ db_connector.py
@@ -0,0 +1,20 @@
+import mysql.connector
+from mysql.connector import Error
+try:
+    connection = mysql.connector.connect(host='127.0.0.1', database='secret', user='lotusp', password='P@ssw0rd0829')
+    if connection.is_connected():
+       db_Info = connection.get_server_info()
+       print("Connected to MySQL database... MySQL Server version on ",db_Info)
+       cursor = connection.cursor()
+       cursor.execute("select database();")
+       record = cursor.fetchone()
+       print ("Your connected to - ", record)
+except Error as e :
+    print ("Error while connecting to MySQL", e)
+finally:
+    #closing database connection.
+    if(connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
+
